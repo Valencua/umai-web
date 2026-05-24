aa74b6ee047aa0b46bc480cce0ff3d06a1f1db4e
Valencua <valencuavasula@gmail.com>
2026-05-24 21:19:09 +0100

    test_base_jinja

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+#from umai.routes.login import login_bp
+#from umai.routes.dashboard import dashboard_bp
+#from umai.routes.historial import historial_bp
+#from umai.routes.abm_menu import abm_menu_bp
+#from umai.routes.abm_reseñas import abm_reseñas_bp
+#from umai.routes.abm_servicios import abm_servicios_bp
+#from umai.routes.escaner import escaner_bp
 
 app = Flask(__name__)
 
-@app.route('/')
-def inicio():
-   
-    return render_template('index.html') 
+
 
 
 if __name__ == '__main__':
