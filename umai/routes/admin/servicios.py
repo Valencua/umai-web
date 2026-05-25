787e390fe5ec48272739de4be27044a419db0e4e
ezequiel <ezeblade@gmail.com>
2026-05-25 14:50:17 -0300

    feat admin: Pantalla servicios

--- umai/routes/admin/servicios.py
+++ umai/routes/admin/servicios.py
@@ -1,3 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 
 abm_servicios_bp = Blueprint('servicios', __name__)
+
+@abm_servicios_bp.route('/', methods=['GET', 'POST'])
+def auth():
+
+    return render_template('admin/servicios.html')
