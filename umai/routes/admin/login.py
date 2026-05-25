93553ddff80829440b07866f4257503933b70c86
ezequiel <ezeblade@gmail.com>
2026-05-25 03:26:51 -0300

    feat admin: pantalla menu

--- umai/routes/admin/login.py
+++ umai/routes/admin/login.py
@@ -5,4 +5,4 @@ login_bp = Blueprint('login', __name__)
 @login_bp.route('/', methods=['GET', 'POST'])
 def auth():
 
-    return render_template('login.html')
+    return render_template('admin/login.html')
--- umai/routes/admin/menu.py
+++ umai/routes/admin/menu.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 
 abm_menu_bp = Blueprint('menu', __name__)
+
+@abm_menu_bp.route('/')
+def index():
+    return render_template('admin/menu.html')
