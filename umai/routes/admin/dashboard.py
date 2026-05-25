217c599813afbbaf6a715622153da677b31eab84
ezequiel <ezeblade@gmail.com>
2026-05-25 03:19:52 -0300

    feat admin: pantalla historial

--- umai/routes/admin/dashboard.py
+++ umai/routes/admin/dashboard.py
@@ -4,4 +4,4 @@ dashboard_bp = Blueprint('dashboard', __name__)
 
 @dashboard_bp.route('/')
 def index():
-    return render_template('dashboard.html')
+    return render_template('admin/dashboard.html')
--- umai/routes/admin/historial.py
+++ umai/routes/admin/historial.py
@@ -1,3 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 
 historial_bp = Blueprint('historial', __name__)
+
+@historial_bp.route('/')
+def index():
+    return render_template('admin/historial.html')
