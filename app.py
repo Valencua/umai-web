3df0f2d4f30d36eb11a0b16370ef40f6a9c635a8
Candela Basaldua <cabasaldua@fi.uba.ar>
2026-05-26 23:29:16 +0000

    feat(reseña): crear y confirmar reseña enviada

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 def inicio():
    
     return render_template('index.html') 
+
+@app.route('/resenas')
+def mostrar_resenas():
+    return render_template('reseñas.html', enviado=False)
+
+@app.route('/resenas/crear', methods=['POST'])
+def crear_resena():
+    puntuacion = request.form.get('rating')
+    comentario = request.form.get('descripcion')
+
+    return render_template('reseñas.html', enviado=True)       
 
 
 if __name__ == '__main__':
