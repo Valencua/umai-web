2bb917cdf5f78d9a9df353230a5bc49e94a0db38
Valencua <valencuavasula@gmail.com>
2026-05-27 22:10:11 +0100

    instagram and tiktok created

--- static/js/admin/escaner.js
+++ static/js/admin/escaner.js
@@ -1,7 +1,3 @@
-/**
- * Sin fetch: la cámara solo lee el QR y envía el formulario HTML (POST al servidor Flask).
- * Flask llama a umai-api: PATCH /reservas/{uuid_codigo} con {"funcion": "confirmar"}.
- */
 
 const UUID_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
