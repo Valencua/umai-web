84b64c3346e066b632c74a1bdfd3c1ff802d20f8
mmolinar77 <mmolinar@fi.uba.ar>
2026-05-29 16:46:17 -0300

    refactor(form): agregar nuevos mensajes de validacion de campos

--- static/js/cliente/form.js
+++ static/js/cliente/form.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    
     const formulario = document.getElementById("formulario-reserva");
     const mensajeError = document.getElementById("mensaje-error");
 
-
     formulario.addEventListener("submit", function(evento) {
         
-        
+       
         const nombre = formulario.nombre.value.trim();
         const email = formulario.email.value.trim();
         const fecha = formulario.fecha.value.trim();
@@ -14,18 +12,55 @@ document.addEventListener("DOMContentLoaded", function() {
         const horario = formulario.horario.value.trim();
         const personas = formulario.personas.value.trim();
 
-  
-        if (!nombre || !email || !fecha || !telefono || !horario || !personas) {
-            
-          
-            evento.preventDefault(); 
-            
-            
+        
+        function mostrarError(mensaje) {
+            mensajeError.innerText = mensaje;
             mensajeError.classList.remove("hidden");
-            
-        } else {
-         
-            mensajeError.classList.add("hidden");
+            evento.preventDefault();
         }
+
+        
+        mensajeError.classList.add("hidden");
+
+        
+        
+        if (nombre === "") {
+            mostrarError("Ingrese un nombre válido /Llene todos los campos");
+            return;
+        }
+
+        if (email === "") {
+            mostrarError("Ingrese un email válido / Llene todos los campos");
+            return;
+        }
+
+        if (fecha === "") {
+            mostrarError("Ingrese una fecha válida / Llene todos los campos");
+            return;
+        }
+
+        
+        const telefonoSinEspacios = telefono.replace(/\s+/g, '');
+        
+      
+        const regexTelefonoArg = /^(?:\+?549)?\d{10}$/;
+
+        if (!regexTelefonoArg.test(telefonoSinEspacios)) {
+            mostrarError("Ingrese un teléfono válido / Llene todos los campos");
+            return;
+        }
+
+        const regexHorario = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+
+        if (!regexHorario.test(horario)) {
+            mostrarError("Ingrese un horario válido / Llene todos los campos");
+            return;
+        }
+
+        if (personas === 0) {
+            mostrarError("Llene todos los campos");
+            return;
+        }
+
     });
 });
