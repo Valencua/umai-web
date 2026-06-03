0b477d76b4ec7003de0ca8912eef2aa6953313c9
ezequiel <ezeblade@gmail.com>
2026-06-03 01:03:17 -0300

    fix: permitir al empleado acceder al dashboard e historial

--- umai/routes/admin/dashboard.py
+++ umai/routes/admin/dashboard.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
-from umai.utils import requiere_admin
+from umai.utils import requiere_login
 dashboard_bp = Blueprint('dashboard', __name__)
 
 @dashboard_bp.route('/')
-@requiere_admin
+@requiere_login
 def index():
     return render_template('admin/dashboard.html')
 
--- umai/routes/admin/historial.py
+++ umai/routes/admin/historial.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
-from umai.utils import requiere_admin
+from umai.utils import requiere_login
 historial_bp = Blueprint('historial', __name__)
 
 @historial_bp.route('/')
-@requiere_admin
+@requiere_login
 def index():
     return render_template('admin/historial.html')
 
