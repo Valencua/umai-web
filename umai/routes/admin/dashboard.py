0979244429be5d9a54b3ad0786268d3a76f17d2a
ezequiel <ezeblade@gmail.com>
2026-06-03 02:03:20 -0300

    fix: Borrar dos lineas no pertenecientes

--- umai/routes/admin/dashboard.py
+++ umai/routes/admin/dashboard.py
@@ -1,12 +1,11 @@
 from flask import Blueprint, render_template
 from umai.services.dashboard import obtener_metricas, obtener_ultimas_reservas
-from umai.utils import requiere_admin
+
 
 dashboard_bp = Blueprint('dashboard', __name__)
 
 
 @dashboard_bp.route('/')
-@requiere_admin
 def index():
     data = obtener_metricas()
     ultimas_reservas = obtener_ultimas_reservas()
