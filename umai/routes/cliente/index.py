b4797982402e4ef8ba68c7b2e5cd01e3f544c33d
Candela Basaldua <cabasaldua@fi.uba.ar>
2026-06-03 21:32:32 +0000

    feat: integracion obtener platos cliente

--- umai/routes/cliente/index.py
+++ umai/routes/cliente/index.py
@@ -1,9 +1,18 @@
-from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask import Blueprint, render_template
+import requests
+from umai.constants import UMAI_API_URL
 
 index_bp = Blueprint('index', __name__)
 
 @index_bp.route('/')
 def index():
-    return render_template('cliente/index.html')
+    try: 
+        response = requests.get(f'{UMAI_API_URL}/platos/', timeout=5)
+        response.raise_for_status()
+        platos = response.json().get('data', [])
+    except Exception:
+        platos = []
+
+    return render_template('cliente/index.html', platos=platos)
 
 
